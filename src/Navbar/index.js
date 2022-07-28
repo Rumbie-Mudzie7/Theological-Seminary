712051ca80e6890ab9c5d41f0a696707f336482c
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-07-28 15:36:08 +0200

    Added dependencies

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -3,8 +3,8 @@ import styles from './Navbar.module.scss';
 
 function Navbar () {
   return (
-    <div className={`${styles.navbar}`} id="nav">
-      <h2>Hello World</h2>
+    <div className= {`${styles.navbar}`} id="nav">
+      <h2 className='text-danger'>Hello World</h2>
       <p>Grace is so cute! :</p>
     </div>
   )
