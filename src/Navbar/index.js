b1a98cb63e90f4a4b4e1b3a46e44d7783c88ad1b
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-08-20 10:20:36 +0200

    Added tabsection

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Logo from '../assets/placeholder-logo.jpg';
 
 function Navbar() {
   return (
-    <div className="container d-flex">
+    <Nav className="container d-flex">
       <img src={Logo} alt="logo" width={100} height={100} />
-      <h1 className="text">Welcome To Nicholas Bhengu Theological Seminary</h1>
-    </div>
+      <NavHeading className="text">Welcome To Nicholas Bhengu Theological Seminary</NavHeading>
+    </Nav>
   );
 }
+
+const Nav = styled.div`
+
+`;
+
+const NavHeading = styled.h1`
+  color: rgb(246, 255, 0);
+`;
 
 export default Navbar;
---
+++ src/Tabsection/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MenuTabs() {
+  return (
+    <div className='menu-section'>MenuTabs</div>
+  )
+}
+
+export default MenuTabs
