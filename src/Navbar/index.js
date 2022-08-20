a20c487d5b2272a074b401711a3315f6fda5cbf6
Clayton Siby <claytonsiby@gmail.com>
2022-08-20 11:04:18 +0200

    Fix: navbar styles

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -4,9 +4,15 @@ import Logo from '../assets/placeholder-logo.jpg';
 
 function Navbar() {
   return (
-    <Nav className="container d-flex">
-      <img src={Logo} alt="logo" width={100} height={100} />
-      <NavHeading className="text">Welcome To Nicholas Bhengu Theological Seminary</NavHeading>
+    <Nav className="container-fluid border-bottom">
+      <div className="row">
+        <div className="col-md-2">
+          <img src={Logo} alt="logo" width={100} height={100} />
+        </div>
+        <div className="col-md-8 text-center">
+          <NavHeading className="text">Welcome To Nicholas Bhengu Theological Seminary</NavHeading>
+        </div>
+      </div>
     </Nav>
   );
 }
