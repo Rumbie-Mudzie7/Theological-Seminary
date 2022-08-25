29e4b96dbcf5fb5bc23f827d022003179dd0d52b
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-08-25 11:56:25 +0200

    Implemented download and button styling

--- src/components/Midsection/index.js
+++ src/components/Midsection/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import 'font-awesome/css/font-awesome.min.css';
+import SampleFile from '../../sampleFile.pdf';
 
 function Midsection() {
   return (
     <>
-      <div>
-        <h1>Applications for 2023 are still open</h1>
+      <Wrapper className="menu-section pt-3">
+        <Button type="button" className="btn w-25">ABOUT US</Button>
+        <Button type="button" className="btn w-25">FEES</Button>
+        <Button type="button" className="btn w-25">FORMS</Button>
+        <Bell className="fa fa-bell" aria-hidden="true" />
+      </Wrapper>
+      <Wrapper className="menu-section">
+        <Button type="button" className="btn w-25">LIBRARY</Button>
+        <Button type="button" className="btn w-25">CONTACT INFO</Button>
+        <Button type="button" className="btn w-25">EVENTS</Button>
+        <a href={SampleFile} target="_blank" rel="noopener noreferrer" download>
+          <Bell className="fa fa-download" aria-hidden="true" />
+          {/* Download */}
+        </a>
+      </Wrapper>
+      <ClickWrapper>
+        <h1 className="text-danger">Applications for 2023 are still open</h1>
         <Button2 type="button" className="btn pull-right">Click here</Button2>
-      </div>
-      <div className="col-md-1">
+      </ClickWrapper>
+      <div className="col-md-1 pt-5">
         <Twitter className="fa fa-twitter-square" aria-hidden="true" />
         <Facebook className="fa fa-facebook-square" aria-hidden="true" />
         <Instagram className="fa fa-instagram" aria-hidden="true" />
@@ -22,24 +38,29 @@ function Midsection() {
   );
 }
 
-// const Wrapper = styled.div`
+const Wrapper = styled.div`
 
-// `;
-// const Button = styled.button`
-//   background-color: red;
-//   color: #fff;
-//   margin: 1rem;
-//   font-weight: bold;
+`;
 
-//   .btn:hover{
-//     color: rgb(255, 255, 255);
-//   }
-// `;
+const ClickWrapper = styled.div`
+ padding: 5rem;
+`;
+const Button = styled.button`
+  background-color: red;
+  color: #fff;
+  margin: 1rem;
+  font-weight: bold;
 
-// const Bell = styled.i`
-//   cursor: pointer;
-//   font-size: 2rem;
-// `;
+  .btn:hover{
+    color: rgb(255, 255, 255);
+  }
+`;
+
+const Bell = styled.i`
+  cursor: pointer;
+  font-size: 2rem;
+  color: #000;
+`;
 
 const Button2 = styled.button`
   background-color: rgb(246, 255, 0);
