6ed6e1ea82a7da19bb5149437ac3dbd6b5d822a1
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-08-25 11:57:13 +0200

    Added padding to push down image

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,7 +3,7 @@ import Principal from '../../assets/principle1.jpg';
 
 function SideBar() {
   return (
-    <div className="pt-3">
+    <div className="pt-4">
       <img src={Principal} alt="principal" width={300} height={200} />
       <h1>Dr Jele Maganyi</h1>
       <h2>Principal</h2>
