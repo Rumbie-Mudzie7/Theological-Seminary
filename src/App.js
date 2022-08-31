055ec793b286a1e9b5a46ddcb3546a1385c464c1
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-08-31 16:35:20 +0200

    Added Applications component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import SideBar from './components/SideBar';
 import Navbar from './components/Navbar';
 import Galary from './components/Galary';
@@ -6,12 +7,13 @@ import Midsection from './components/Midsection';
 import LeftInfo from './components/InfoSection/menu';
 import RightInfo from './side';
 import './main.css';
+import AppSection from './components/Applications';
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="container-fluid">
+      <MidSection className="container-fluid">
         <div className="row">
           <div className="col-12 col-md-3">
             <SideBar />
@@ -23,8 +25,8 @@ function App() {
             <Galary />
           </div>
         </div>
-      </div>
-      <div className="container-fluid">
+      </MidSection>
+      <div className="container-fluid pt-5">
         <div className="row">
           <div className="col-12 col-md-4">
             <LeftInfo />
@@ -34,8 +36,13 @@ function App() {
           </div>
         </div>
       </div>
+      <AppSection />
     </>
   );
 }
+
+const MidSection = styled.div`
+  background-color: #EFEFEF;
+`;
 
 export default App;
