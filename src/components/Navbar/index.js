009a5ea21a4d638879554bf1c1f3695971c51b72
Rumbie-Mudzie7 <rumbiemudzie@gmail.com>
2022-11-25 01:27:50 +0200

    Added animation on logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import Logo from '../../assets/nbts-logo.png';
 import Logo2 from '../../assets/africa-logo.png';
+// import './Navbar.module.scss';
 import './Navbar.module.scss';
 
 function Navbar() {
   return (
     <Nav className="container-fluid border-bottom">
       <div className="row">
-        <FirstDiv className="event col-md-2">
-          <img src={Logo} alt="logo" id="sweetlandia" width={120} height={120} />
+        <FirstDiv className="col-md-2">
+          <img src={Logo} alt="logo" id="logo" width={120} height={120} />
         </FirstDiv>
         <div className="nav col-md-8 text-center">
           <NavHeading className="text">Welcome To Nicholas Bhengu Theological Seminary</NavHeading>
@@ -28,6 +29,16 @@ const Nav = styled.div`
 
 const FirstDiv = styled.div`
   padding-left: 4rem;
+  @keyframes swing {
+    0%, 100% { transform: rotate(-30deg); }
+    20% {transform:scale(.95);}
+    50% { transform: rotate(30deg); }
+    80% {transform:scale(.95);}
+  }
+
+  #logo {
+    animation: swing 2s infinite ease-in-out;
+  }
 `;
 
 const SecondDiv = styled.div`
@@ -37,7 +48,7 @@ const NavHeading = styled.h1`
   position: relative;
   top: 12px;
   right: 50px;
-  color: rgb(246, 255, 0);
+  color: #0B69AE;
   font-size: 2.8rem;
   font-weight: bold;
 `;
