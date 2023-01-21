11ab591e9ad6968f3d5f7d2d6e75692550f9761b
Clayton Siby <claytonsiby@gmail.com>
2023-01-21 14:30:35 +0200

    Fix: some issues

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const InputField = ({ inputFieldName }) => (
+const InputField = ({ inputFieldId, inputFieldType, inputFieldName }) => (
   <div>
-    <label htmlFor={`${inputFieldName}`}>{`${inputFieldName}`}</label>
-    <input type="text" placeholder="Enter your name" id={`${inputFieldName}`} />
+    <label htmlFor={`${inputFieldId}`}>{`${inputFieldName}`}</label>
+    <input type={`${inputFieldType}`} id={`${inputFieldId}`} />
   </div>
 );
 
--- src/pages/InforFormPage.js
+++ src/pages/InforFormPage.js
@@ -7,10 +7,13 @@ const InforFormPage = () => (
   <div className="container">
     <InfoPageHeader />
     <form>
-      <InputField inputFieldName="Tittle" />
-      <InputField inputFieldName="Surname" />
-      <InputField inputFieldName="Full Name" />
-      <TextArea textAreaID="address" textAreaTitle="Home Address" />
+      <fieldset>
+        <InputField inputFieldId="title" inputFieldType="text" inputFieldName="Tittle" />
+        <InputField inputFieldId="surname" inputFieldType="text" inputFieldName="Surname" />
+        <InputField inputFieldId="full_name" inputFieldType="text" inputFieldName="Full Name" />
+        <TextArea textAreaID="address" textAreaTitle="Home Address" />
+        <InputField inputFieldId="email" inputFieldType="email" inputFieldName="Email" />
+      </fieldset>
     </form>
   </div>
 );
